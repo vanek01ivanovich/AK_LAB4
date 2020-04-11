008fcfa77d33da69eb4b1b21af39be67f523b6d4
ivan <ivan.kuchkov17@gmail.com>
2020-04-11 23:56:50 +0300

    lab4 AK

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ivan/repos/busybox/_install/examples/test1/hello.ko := ccache arm-eabi-ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/ivan/repos/busybox/_install/examples/test1/hello.ko /home/ivan/repos/busybox/_install/examples/test1/hello.o /home/ivan/repos/busybox/_install/examples/test1/hello.mod.o ;  true
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/ivan/repos/busybox/_install/examples/test1/hello.ko
+/home/ivan/repos/busybox/_install/examples/test1/hello.o
+
